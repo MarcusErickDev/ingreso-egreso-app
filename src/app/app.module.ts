47bfe7a2adab5de7c78ffeb9c7b0e3086db5442f
Marcus Erick <marcushp1@hotmail.com>
2026-04-15 20:49:55 -0600

    register form signin

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,15 @@
+import { environment } from '../environments/environment';
+
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { AppRoutingModule } from './app.routing.module';
 import { ReactiveFormsModule } from '@angular/forms';
+// AngularFire compat
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+import { AngularFireAuthModule } from '@angular/fire/compat/auth';
+// import { AngularFireDatabaseModule } from '@angular/fire/compat/database';
 
+import { AppRoutingModule } from './app.routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './auth/login/login.component';
 import { RegisterComponent } from './auth/register/register.component';
@@ -30,9 +37,53 @@ import { SidebarComponent } from './shared/sidebar/sidebar.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule,
+    AngularFireAuthModule,
+    // AngularFireDatabaseModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+
+// import { NgModule } from '@angular/core';
+// import { BrowserModule } from '@angular/platform-browser';
+// import { AppRoutingModule } from './app.routing.module';
+// import { ReactiveFormsModule } from '@angular/forms';
+
+// import { AppComponent } from './app.component';
+// import { LoginComponent } from './auth/login/login.component';
+// import { RegisterComponent } from './auth/register/register.component';
+// import { DashboardComponent } from './dashboard/dashboard.component';
+// import { IngresoEgresoComponent } from './ingreso-egreso/ingreso-egreso.component';
+// import { EstadisticaComponent } from './ingreso-egreso/estadistica/estadistica.component';
+// import { DetalleComponent } from './ingreso-egreso/detalle/detalle.component';
+// import { FooterComponent } from './shared/footer/footer.component';
+// import { NavbarComponent } from './shared/navbar/navbar.component';
+// import { SidebarComponent } from './shared/sidebar/sidebar.component';
+
+// @NgModule({
+//   declarations: [
+//     AppComponent,
+//     LoginComponent,
+//     RegisterComponent,
+//     DashboardComponent,
+//     IngresoEgresoComponent,
+//     EstadisticaComponent,
+//     DetalleComponent,
+//     FooterComponent,
+//     NavbarComponent,
+//     SidebarComponent
+//   ],
+//   imports: [
+//     BrowserModule,
+//     AppRoutingModule,
+//     ReactiveFormsModule
+//   ],
+//   providers: [],
+//   bootstrap: [AppComponent]
+// })
+// export class AppModule { }
---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyBV0ogVvDuSULC5x6Mltr3SiDSlS9bsOgk",
+    authDomain: "ingreso-egreso-app-siontech.firebaseapp.com",
+    projectId: "ingreso-egreso-app-siontech",
+    storageBucket: "ingreso-egreso-app-siontech.firebasestorage.app",
+    messagingSenderId: "121440604248",
+    appId: "1:121440604248:web:ed4f6eac0245c1cadffa2d",
+    // databaseURL: 'TU_DATABASE_URL'
+  }
+};
