9d0138fea98a0bc5f0def848e8cfac1705a1ce35
Marcus Erick <marcushp1@hotmail.com>
2026-04-19 23:27:05 -0600

    estadistica module

--- src/app/ingreso-egreso/estadistica/estadistica.component.ts
+++ src/app/ingreso-egreso/estadistica/estadistica.component.ts
@@ -1,9 +1,49 @@
 import { Component } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { AppState } from 'src/app/app.reducer';
+import { IngresoEgreso } from 'src/app/models/ingreso-egreso.model';
 
 @Component({
   selector: 'app-estadistica',
   templateUrl: './estadistica.component.html'
 })
 export class EstadisticaComponent {
+
+  ingresos:number = 0;
+  egresos :number = 0;
+
+  totalEgresos :number = 0;
+  totalIngresos:number = 0;
+
+  // public doughnutChartLabels: Label[] = ['Ingresos', 'Egresos'];
+  // public doughnutChartData: MultiDataSet = [[]];
+
+  constructor( private store: Store<AppState> ) {}
+
+  ngOnInit() {
+    this.store.select('ingresoEgreso')
+      .subscribe( ({ items }) => this.generarEstadistica( items ) );
+  }
+
+  generarEstadistica( items: IngresoEgreso[] ) {
+
+    this.totalEgresos  = 0;
+    this.totalIngresos = 0;
+    this.ingresos = 0;
+    this.egresos  = 0
+
+    for (const item of items ) {
+      if ( item.tipo === 'ingreso' ) {
+        this.totalIngresos += item.monto;
+        this.ingresos ++;
+      } else {
+        this.totalEgresos += item.monto;
+        this.egresos ++;
+      }
+    }
+
+    // this.doughnutChartData = [ [this.totalIngresos, this.totalEgresos] ];
+
+  }
 
 }
--- src/app/shared/sidebar/sidebar.component.ts
+++ src/app/shared/sidebar/sidebar.component.ts
@@ -1,16 +1,33 @@
 import { Router } from '@angular/router';
 import { AuthService } from 'src/app/services/auth.service';
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { AppState } from 'src/app/app.reducer';
+import { filter, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html'
 })
-export class SidebarComponent {
+export class SidebarComponent implements OnInit, OnDestroy {
+
+  nombre: string = '';
+  userSubs!: Subscription;
 
   constructor( private authService: AuthService,
-               private router: Router
-   ){}
+               private router: Router,
+               private store: Store<AppState> ) { }
+
+  ngOnInit() {
+    this.userSubs = this.store.select('user')
+    .pipe(
+      filter( ({user}) => user != null )
+    )
+    .subscribe( ({ user }) => this.nombre = user!.nombre );
+  }
+  ngOnDestroy(): void {
+    throw new Error('Method not implemented.');
+  }
 
   logout(){
     this.authService.logOut()
